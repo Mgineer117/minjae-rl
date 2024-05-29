94f54f876923071a55f0b373c57b454246320860
Minjae Cho <whalswo117@gmail.com>
2024-05-29 18:30:22 -0500

    ss

--- rlkit/policy_trainer/__init__.py
+++ rlkit/policy_trainer/__init__.py
@@ -1,9 +1,11 @@
 from rlkit.policy_trainer.mf_policy_trainer import MFPolicyTrainer
 from rlkit.policy_trainer.mf_policy_trainer_skill import MFSkillPolicyTrainer
+from rlkit.policy_trainer.mf_policy_trainer_mse import MFMSEPolicyTrainer
 from rlkit.policy_trainer.mb_policy_trainer import MBPolicyTrainer
 
 __all__ = [
     "MFPolicyTrainer",
     "MFSkillPolicyTrainer",
+    "MFMSEPolicyTrainer",
     "MBPolicyTrainer"
 ]
